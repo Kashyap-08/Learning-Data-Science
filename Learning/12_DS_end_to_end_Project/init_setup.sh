c9c9b1404dc26e0e0b35f926fb9346247779b0a9
kashyap kolhe <kpkolhe1998@gmail.com>
2023-10-27 22:23:40 +0530

    New changes

---
+++ Learning/12_DS_end_to_end_Project/init_setup.sh
@@ -0,0 +1,15 @@
+echo [$(date)]: "START"
+
+echo [$(date)]: "CREATING VENV WITH PYTHON 3.9 VERSION"
+
+conda create -p ./venv python==3.9 -y
+
+echo [$(date)]: "ACTIVATING THE ENVIRONMENT"
+
+conda activate ./venv
+
+echo [$(date)]: "INSTALLING LIBRARIES FROM requirement.txt"
+
+pip install -r requirement.txt
+
+echo [$(date)]: "END"
